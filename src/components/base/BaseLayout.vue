f9d1dbd0975ef2e59a2ae5c3cb9a94efa91101eb
youssef chlendi <youssef.chlendi@gmail.com>
2023-02-14 08:40:48 +0100

    create base layout

---
+++ src/components/base/BaseLayout.vue
@@ -0,0 +1,48 @@
+<template>
+	<ion-page>
+		<ion-header>
+			<ion-toolbar>
+				<ion-title>{{ pageTitle }}</ion-title>
+				
+				<ion-buttons slot="start">
+					<ion-back-button :default-href="pageDefaultBackLink">
+					</ion-back-button>
+				</ion-buttons>
+			</ion-toolbar>
+		</ion-header>
+		<ion-content>
+			<slot />
+		</ion-content>
+	</ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonBackButton, IonButtons } from "@ionic/vue";
+export default defineComponent({
+	props: {
+		pageTitle: {
+			type: String,
+			required: true
+		},
+		pageDefaultBackLink: {
+			type: String,
+			required: false
+		}
+	},
+	components:{
+			IonPage,
+			IonHeader,
+			IonToolbar,
+			IonTitle,
+			IonContent,
+			IonButtons,
+			IonBackButton
+		},
+	
+})
+</script>
+
+<style scoped>
+
+</style>
