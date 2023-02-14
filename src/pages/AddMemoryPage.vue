72b04d3b96be84a9c42b6b9e590aaf81b9c1d2e6
youssef chlendi <youssef.chlendi@gmail.com>
2023-02-14 10:37:15 +0100

    add memory

---
+++ src/pages/AddMemoryPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <base-layout page-title="Add a Memory" page-default-back-link="/memories">
+	<memory-form @save-memory="saveMemory"/>
+  </base-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import MemoryForm from '@/components/memories/MemoryForm.vue';
+import { Memory } from '@/models/memory.model';
+import { useMemoryStore } from '@/store/memories.store';
+import { useRouter } from 'vue-router';
+export default defineComponent({
+	components: {
+		MemoryForm
+	},	
+	setup () {
+		const store = useMemoryStore();
+		const router = useRouter();	
+		const saveMemory = (memory: Memory) => {
+			store.addMemory(memory);
+			router.replace('/memories');
+		}	
+
+		return {
+			saveMemory
+		}
+	}
+})
+</script>
+
+<style scoped>
+
+</style>
